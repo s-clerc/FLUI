e77ae3774bfe6dd5503edf013d57392d8cc16e40
Samarth AGARWAL <samu.agarwal@gmail.com>
2013-08-10 08:12:22 +0200

    remove 1cm min-size because on some moniters it's the size of an inch

--- source/js/button.js
+++ source/js/button.js
@@ -1,29 +1,24 @@
 (function() {
   "use strict";
-  //*Like a normal button with ontap events.
-  var addStyles = function (that) {
-    fl.addOnTap(that);
-    that.style.minHeight = "1cm";
-    that.style.minWidth = "1cm";
-    that.style.display = "inline-block";
-  }
   var xobject = new fl.xtagObject(); 
   xobject.mixins.push("disabled");
   xobject.lifecycle.created = function () {
-    addStyles(this)
+    fl.addOnTap(this);
+    this.style.display = "inline-block";
     this.style.cursor = "pointer";
   }
   //* Just like above, except with no default look
   xtag.register("fl-tappable", xobject);
   xobject.lifecycle.created = function () {
-    addStyles(this);
+    fl.addOnTap(this);
+    this.style.display = "inline-block";
     this.classList.add("btn");
-    console.log(this);
   }
   xobject.accessors.disabled = {};
   xobject.accessors.disabled.set = function(value) {
     (xtag.mixins.disabled.accessors.disabled.set.bind(this))()
     this.classList[value ? "add":"remove"]("disabled");
     }
+  //*Like a normal button with ontap events.
   xtag.register("fl-button", xobject)
 }());
