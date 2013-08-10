5463dced784d74bdc4b8d20d5f98e2da024e0cb1
Samarth AGARWAL <samu.agarwal@gmail.com>
2013-08-10 10:32:27 +0200

    Fix bug in ontap event where the event would fire twice when tapped with finger

--- source/js/lib2.js
+++ source/js/lib2.js
@@ -1,22 +1,39 @@
 //Non-strict code
 (function() {
- var fireOnTap = function (e) {
+  //* Boilerplate for xtag components
+  window.fl.xtagObject = function () {
+    this.lifecycle = {};
+    this.events = {};
+    this.accessors = {};
+    this.methods = {};
+    this.mixins = [];
+  }
+  var fireOnTap = function (e) {
     xtag.fireEvent(this, "tap", e);
+    r = true;
     if (this.ontap) {
       if (this.ontap(e) === false) {
-        return false;
+        r = false;
       }
     } 
     if (this.getAttribute("ontap")) {
       eval(this.getAttribute("ontap"));
     }
+    if (e.type === "touchend") {
+      e.preventDefault();
+    }
+    return r;
   };
+  //* Adds the ontap event.
+  //* When using inline handlers the event object is unavaliable.
   window.fl.addOnTap = function (n) {
     n.addEventListener("touchend", fireOnTap, false);
     n.addEventListener("mouseup", fireOnTap, false);        
   };
+  //* Removes the ontap event.
   window.fl.removeOnTap = function (n) {
     n.removeEventListener("touchend", fireOnTap, false);
     n.removeEventListener("mouseup", fireOnTap, false);          
   }
+
 }());
