cb99653d36739903b6e283bf218dfb2077ae70ed
Samarth AGARWAL <samu.agarwal@gmail.com>
2013-08-10 08:00:06 +0200

    Finish fl-button

--- source/js/button.js
+++ source/js/button.js
@@ -1,35 +1,29 @@
 (function() {
   "use strict";
   //*Like a normal button with ontap events.
-  //*It's touch target is bigger than it's visible size
-  xtag.register("fl-button", {
-    lifecycle: {
-      created: function() {
-        this.xtag.span = document.createElement("span");
-        this.xtag.extendObject = new fl.extendEmulationObject(this, this.xtag.span, ["class", "style", "unselectable"]);
-        this.xtag.extendObject.copyAttributes(true, [/^on/, "id", "class", "style"]);
-        fl.addOnTap(this);
-        this.style.minHeight = "44px";
-        this.style.minWidth = "44px";
-        this.style.display = "inline-block";
-        this.style.cursor = "pointer";
-        this.xtag.span.classList.add("btn");
-        fl.appendChildren(this.childNodes, this.xtag.span);
-        this.appendChild(this.xtag.span);
-        this.xtag.extendObject.emulate();
-      }
+  var addStyles = function (that) {
+    fl.addOnTap(that);
+    that.style.minHeight = "1cm";
+    that.style.minWidth = "1cm";
+    that.style.display = "inline-block";
+  }
+  var xobject = new fl.xtagObject(); 
+  xobject.mixins.push("disabled");
+  xobject.lifecycle.created = function () {
+    addStyles(this)
+    this.style.cursor = "pointer";
+  }
+  //* Just like above, except with no default look
+  xtag.register("fl-tappable", xobject);
+  xobject.lifecycle.created = function () {
+    addStyles(this);
+    this.classList.add("btn");
+    console.log(this);
+  }
+  xobject.accessors.disabled = {};
+  xobject.accessors.disabled.set = function(value) {
+    (xtag.mixins.disabled.accessors.disabled.set.bind(this))()
+    this.classList[value ? "add":"remove"]("disabled");
     }
-  });
-  //*Just like above, except with no default styling
-  xtag.register("fl-tappable", {
-    lifecycle: {
-      created: function() {
-        fl.addOnTap(this);
-        this.style.minHeight = "44px";
-        this.style.minWidth = "44px";
-        this.style.display = "inline-block";
-        this.style.cursor = "pointer";
-      }
-    }
-  });
+  xtag.register("fl-button", xobject)
 }());
