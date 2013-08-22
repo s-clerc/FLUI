702538dca16339c7ca4b041c498430e24981cf1a
Samarth AGARWAL <samu.agarwal@gmail.com>
2013-08-22 12:50:32 +0200

    Add tests for fl.forEach

--- tests/spec/FluiSpec.js
+++ tests/spec/FluiSpec.js
@@ -5,7 +5,7 @@
       it("should be an object", function () {
         expect(typeof fl).toBe("object");
       });
-      describe("xtagObject.", function () {
+      describe("xtagObject", function () {
         var xobject;
         beforeEach(function() {
           xobject = new fl.xtagObject();
@@ -64,14 +64,14 @@
           beforeEach(function () {
             span.addEventListener("tap", function () {
               window.i = true;
-            });
+            }, false);
           });
           doTests();
         });
         describe("fl.removeOnTap", function () {
           beforeEach(function () {
             fl.removeOnTap(span);
-            span.addEventListener(function () {
+            span.addEventListener("tap", function () {
               window.i = true;
             });
           });
@@ -87,9 +87,64 @@
           });
         });
         it("Make sure the test teardown was done properly, other tests may be inaccurate if this fails", function () {
-          expect(window.i).not.toBeDefined();
+          expect(window.i).toBeUndefined();
         });
       });
+      describe("forEach", function () {
+        it("shouldn't work without a callback", function () {
+          var test = function () {
+            fl.forEach([]);
+          };
+          expect(test).toThrow();
+        });
+        it("shouldn't work when the callback is a function", function () {
+          var test = function () {
+            fl.forEach([], {});
+          };
+          expect(test).toThrow();
+        });
+        it("shouldn't work when there isn't an arraylike passed to it", function () {
+          var test = function () {
+            fl.forEach(false, function() {});
+          };
+          expect(test).toThrow();
+        });
+        it("shouldn't work when an object without the .length property is passed", function () {
+          var test = function () {
+            fl.forEach({}, function () {});
+          };
+          expect(test).toThrow();
+        });
+        it("shouldn't work when an object without the ability to query with []", function () {
+          var test = function () {
+            fl.forEach({length: 1}, function () {});
+          };
+          expect(test).toThrow();
+        });
+        it("should execute the callback * array.length", function () {
+          var i = 0;
+          fl.forEach([1,2,3,4,5,6,7], function () {
+            i += 1;  
+          });
+          expect(i).toBe(7)
+        });
+        it("should return true when it has gone through the whole array", function () {
+          expect(fl.forEach([1], function () {})).toBeTruthy();
+        });
+        it("should stop going through the array when the callback returns false", function () {
+          i = 0;
+          fl.forEach([1,2,3,4], function () {
+            i += 3;
+            if(i===3) {
+              return false;
+            }
+          });
+          expect(i).toBe(3);
+        });
+        it("should return false when looping is interupted by return false", function () {
+          expect(fl.forEach([1, 2], function () {return false;})).toBeFalsy();
+        });
+      })
     });
   });
 }());
